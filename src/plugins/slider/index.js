6ebbadc0098d0a47c704e4cc362e9a6ab24a0649
webfarrock <webfarrock@mail.ru>
2021-12-16 14:27:22 +0300

    code for slider

--- src/plugins/slider/index.js
+++ src/plugins/slider/index.js
@@ -1,3 +1,58 @@
 import "./index.scss";
 
-console.log("hi from slider js");
+/* eslint func-names: ["error", "never"] */
+(function () {
+  function sliderEnhancer(slider) {
+    let slideIndex = 1;
+    const { autoStart } = slider.dataset;
+    const interval = slider.dataset.interval ? slider.dataset.interval * 1000 : 4000;
+    const btnPrev = slider.querySelector(".plugin-slider-prev");
+    const btnNext = slider.querySelector(".plugin-slider-next");
+
+    function showSlides(n) {
+      const slides = Array.from(slider.getElementsByClassName("plugin-slider-item"));
+      console.log("slides: ", slides);
+
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+
+      slides.forEach((slide) => {
+        const slideItem = slide;
+        slideItem.style.display = "none";
+      });
+
+      slides[slideIndex - 1].style.display = "block";
+    }
+
+    if (autoStart) {
+      setInterval(() => {
+        showSlides((slideIndex += 1));
+      }, interval);
+    }
+
+    function nextSlide() {
+      showSlides((slideIndex += 1));
+    }
+
+    function previousSlide() {
+      showSlides((slideIndex -= 1));
+    }
+
+    btnPrev.addEventListener("click", previousSlide);
+    btnNext.addEventListener("click", nextSlide);
+  }
+
+  function init() {
+    const sliderElems = document.querySelectorAll(".plugin-slider");
+    sliderElems.forEach((slider) => {
+      sliderEnhancer(slider);
+    });
+  }
+
+  window.addEventListener("load", init);
+})();
